2b9331db1d5440920a603fa4b45b3e41108a27f1
Harrycarter555 <top9pdf@gmail.com>
2026-01-10 02:29:45 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
+// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSy....",
@@ -7,8 +9,10 @@ const firebaseConfig = {
   projectId: "reelearn-505d9",
   storageBucket: "reelearn-505d9.appspot.com",
   messagingSenderId: "207249486424",
-  appId: "1:207249486424:web:63461de258102164f8102d"
+  appId: "1:207249486424:web:63461de258102164f8102d",
+  measurementId: "G-KW2KQF47GQ"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
