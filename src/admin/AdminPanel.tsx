844ed1d2d157137b64a977747922260ad72dca65
Harrycarter555 <top9pdf@gmail.com>
2026-01-11 00:17:36 +0530

    Create AdminPanel.tsx

---
+++ src/admin/AdminPanel.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Campaign } from "../types";
+
+const AdminPanel: React.FC<any> = ({
+  appState,
+  setAppState,
+  adminTab,
+  setAdminTab
+}) => {
+  return (
+    <div className="pb-40">
+      {/* ADMIN JSX — handleFile, handleCreateCampaign, dashboard etc */}
+    </div>
+  );
+};
+
+export default AdminPanel;
