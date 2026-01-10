12b5fcef0360e1717cd9609955090e517aa2162f
Harrycarter555 <top9pdf@gmail.com>
2026-01-11 00:54:53 +0530

    Delete src/state directory

--- src/state/userProfile.ts
+++
@@ -1,7 +0,0 @@
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../firebase";
-
-export async function getUserProfile(uid: string) {
-  const snap = await getDoc(doc(db, "users", uid));
-  return snap.exists() ? snap.data() : null;
-}
